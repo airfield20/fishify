130390617e2aa720ff251c829f1159637f203cc0
Aaron Cofield <afcofiel@umich.edu>
2017-06-18 13:00:45 -0400

    Initial Commit

---
+++ installFish.sh
@@ -0,0 +1,8 @@
+sudo apt-get install fish #install fish
+echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
+fish
+curl -L https://get.oh-my.fish | fish #install oh my fish
+omf install bobthefish
+omf theme bobthefish
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
