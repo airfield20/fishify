0cea2e0cc6d12430efad76c1c5e5c0ac987e1e1c
Aaron Cofield <afcofiel@umich.edu>
2017-06-18 13:59:00 -0400

    change to zish theme

--- installFish.sh
+++ installFish.sh
@@ -2,7 +2,7 @@ sudo apt-get install fish #install fish
 echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
 fish
 curl -L https://get.oh-my.fish | fish #install oh my fish
-omf install flash
-omf theme flash
+omf install zish
+omf theme zish
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
