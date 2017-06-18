90aa929d6d698547aa46832e86ffb11871b1cb8f
Aaron Cofield <afcofiel@umich.edu>
2017-06-18 14:13:39 -0400

    added thefuck

--- installFish.sh
+++ installFish.sh
@@ -1,8 +1,10 @@
 sudo apt-get install fish #install fish
+sudo apt update
+sudo apt install python3-dev python3-pip
+sudo pip3 install thefuck
 echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
 fish
 curl -L https://get.oh-my.fish | fish #install oh my fish
 omf install flash
 omf theme flash
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
+omf install thefuck
