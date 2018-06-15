4101813e400e0654036b465380a2e2d3371e10d2
aaron <afcofiel@umich.edu>
2018-06-15 17:17:57 -0400

    Fixed scripts to setup defualt fish installation

--- installFish.sh
+++ installFish.sh
@@ -1,10 +1,6 @@
-sudo apt-get install fish #install fish
+sudo apt-get install git fish #install fish
 sudo apt update
-sudo apt install python3-dev python3-pip
-sudo pip3 install thefuck
 echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
+fish get_omf.fish #install oh my fish
+fish zish.fish
 fish
-curl -L https://get.oh-my.fish | fish #install oh my fish
-omf install flash
-omf theme flash
-omf install thefuck
