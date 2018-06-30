a7d97a3bbcbf025e81169de8965e3ced7f2dc43d
aaron <afcofiel@umich.edu>
2018-06-30 08:16:31 -0400

    Added -y to apt install

--- installFish.sh
+++ installFish.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git fish #install fish
+sudo apt-get install -y git fish #install fish
 sudo apt update
 echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
 fish get_omf.fish #install oh my fish
