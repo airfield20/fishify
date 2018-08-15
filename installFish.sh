a4756709694e0bdf6300a412ca9da8831b66b0e8
aaron <aaron@localhost.localdomain>
2018-08-15 00:08:47 +0000

    added fish ppa because new ubuntu doesnt have fish by default

--- installFish.sh
+++ installFish.sh
@@ -1,4 +1,6 @@
-sudo apt-get install -y git fish #install fish
+sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt update
+sudo apt-get install -y fish #install fish
 sudo apt update
 echo "fish" >> ~/.bashrc #start fish whenever you start a new bash
 fish get_omf.fish #install oh my fish
